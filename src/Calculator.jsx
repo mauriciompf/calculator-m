f1add596dca095b9205c7f3c2d3dfaa34b8db09c
mpfarias <fariasmauricio11@outlook.com>
2024-04-16 21:30:17 -0300

    Update files

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -7,6 +7,8 @@ export default function Calculator() {
   let lastAddedItem = items[items.length - 1];
   const plusIcon = " + ";
   let minusIcon = " - ";
+  const productIcon = " x ";
+  const divideIcon = " / ";
 
   function CreateNumbers({ onClick }) {
     let digitNumber = [7, 8, 9, 4, 5, 6, 3, 2, 1, 0];
@@ -25,75 +27,107 @@ export default function Calculator() {
   }
 
   function handlePlusClick() {
-    if (items.length === 0) return;
+    if (
+      items.length === 0 ||
+      lastAddedItem === "-" ||
+      lastAddedItem === plusIcon
+    )
+      items.pop();
+    return;
 
-    if (lastAddedItem !== plusIcon) {
-      setItems((prevState) => [...prevState, plusIcon]);
-    }
-
-    if (lastAddedItem === minusIcon) {
+    if (lastAddedItem === minusIcon || lastAddedItem === productIcon) {
       items.pop();
     }
 
-    if (lastAddedItem === "-") {
-      items.pop();
-    }
+    setItems((prevState) => [...prevState, plusIcon]);
   }
 
   function handleMinusClick() {
-    if (items.length === 0) {
+    if (
+      items.length === 0 ||
+      lastAddedItem === productIcon ||
+      lastAddedItem === "-"
+    ) {
       minusIcon = "-";
-    }
-
-    if (lastAddedItem !== "-") {
-      if (lastAddedItem !== minusIcon) {
-        setItems((prevState) => [...prevState, minusIcon]);
-      }
     }
 
     if (lastAddedItem === plusIcon) {
       items.pop();
     }
+
+    if (lastAddedItem !== minusIcon) {
+      setItems((prevState) => [...prevState, minusIcon]);
+    }
+  }
+
+  function handleProductClick() {
+    if (items.length === 0 || lastAddedItem === productIcon) return;
+
+    if (lastAddedItem === plusIcon || lastAddedItem === minusIcon) {
+      items.pop();
+    } else if (lastAddedItem === "-") {
+      items.pop();
+      items.pop();
+    }
+
+    setItems((prevState) => [...prevState, productIcon]);
   }
 
   function handleResultClick() {
     console.log(items);
 
-    if (items[0] === plusIcon) {
-      items.shift();
-    }
+    // const firstIconIndex = items.findIndex((item) =>
+    //   [plusIcon, minusIcon, productIcon].includes(item)
+    // );
+
+    // if (firstIconIndex !== -1) {
+    //   items.slice(firstIconIndex, 1);
+    // }
+
+    // if (items[0] === plusIcon) {
+    //   items.shift();
+    // }
+
+    // if (items[0] === productIcon) {
+    //   items.shift();
+    // }
+
+    if (lastAddedItem === "-") return;
 
     // Check if icon exist
-
-    const containsPlusIcon = items.some((e) => e === plusIcon);
-    const containsMinusIcon = items.some((e) => e === minusIcon);
+    const containsPlusIcon = items.includes(plusIcon);
+    const containsMinusIcon = items.includes(minusIcon);
+    const containsProductIcon = items.includes(productIcon);
 
     // Convert all strings elements into numbers types
-
-    let numbers = [];
     const itemsString = items.join("");
+    let numbers = [];
 
     if (containsPlusIcon) {
       numbers = itemsString.split(plusIcon).map((str) => parseFloat(str));
-    }
-
-    if (containsMinusIcon) {
+    } else if (containsMinusIcon) {
       numbers = itemsString.split(minusIcon).map((str) => parseFloat(str));
+    } else if (containsProductIcon) {
+      numbers = itemsString.split(productIcon).map((str) => parseFloat(str));
     }
 
     if (numbers.length === 0) return;
 
-    if (lastAddedItem !== plusIcon && lastAddedItem !== minusIcon) {
-      // Sum the numbers
-      const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+    const diff = numbers.reduce((acc, cur) => acc - cur);
+    const product = numbers.reduce((acc, cur) => acc * cur);
 
-      // Diff the numbers
-      const diff = numbers.reduce((acc, cur) => acc - cur);
-
+    if (
+      lastAddedItem !== plusIcon &&
+      lastAddedItem !== minusIcon &&
+      lastAddedItem !== productIcon
+    ) {
       if (containsPlusIcon) {
         setItems([sum]);
       } else if (containsMinusIcon) {
         setItems([diff]);
+      } else if (containsProductIcon) {
+        setItems([product]);
       }
     }
   }
@@ -108,19 +142,19 @@ export default function Calculator() {
           <strong>{items}</strong>
         </div>
         <div className="btn-wrapper">
-          <div>
+          <div className="top">
             <button>AC</button>
             <button>()</button>
             <button>%</button>
             <button>/</button>
           </div>
-          <div>
+          <div className="col-digit">
             <CreateNumbers onClick={handleNumberClick} />
             <button>.</button>
             <button> &lt; </button>
           </div>
-          <div className="">
-            <button>x</button>
+          <div className="col-4">
+            <button onClick={handleProductClick}>x</button>
             <button onClick={handlePlusClick}>+</button>
             <button onClick={handleMinusClick}>-</button>
             <button onClick={handleResultClick}>=</button>
