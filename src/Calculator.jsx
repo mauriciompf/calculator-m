a6bd6bc7239f000a6b4b59824f20c02749b5bb02
mpfarias <fariasmauricio11@outlook.com>
2024-04-22 21:30:17 -0300

    Update files

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -5,6 +5,7 @@ export default function Calculator() {
   const [items, setItems] = useState([]);
 
   let lastAddedItem = items[items.length - 1];
+  let expression = items.join("");
   const plusIcon = " + ";
   let minusIcon = " - ";
   const productIcon = " x ";
@@ -102,8 +103,24 @@ export default function Calculator() {
     setItems((prevState) => [...prevState, divideIcon]);
   }
 
+  console.log(items);
+
   function handleResultClick() {
     if (lastAddedItem === "-") return;
+
+    // Parentheses
+    expression = expression.replace(/\)(\d)/g, ")*$1");
+    expression = expression.replace(/x/g, "*");
+
+    const parenthesisRegex = /\([^()]*\)/g;
+
+    while (parenthesisRegex.test(expression)) {
+      expression = expression.replace(parenthesisRegex, (match) => {
+        return eval(match);
+      });
+    }
+
+    setItems([eval(expression)]);
 
     // Check if icon exist
     const containsPlusIcon = items.includes(plusIcon);
@@ -112,17 +129,16 @@ export default function Calculator() {
     const containsDivideIcon = items.includes(divideIcon);
 
     // Convert all strings elements into numbers types
-    const itemsString = items.join("");
     let numbers = [];
 
     if (containsPlusIcon) {
-      numbers = itemsString.split(plusIcon).map((str) => parseFloat(str));
+      numbers = expression.split(plusIcon).map((str) => parseFloat(str));
     } else if (containsMinusIcon) {
-      numbers = itemsString.split(minusIcon).map((str) => parseFloat(str));
+      numbers = expression.split(minusIcon).map((str) => parseFloat(str));
     } else if (containsProductIcon) {
-      numbers = itemsString.split(productIcon).map((str) => parseFloat(str));
+      numbers = expression.split(productIcon).map((str) => parseFloat(str));
     } else if (containsDivideIcon) {
-      numbers = itemsString.split(divideIcon).map((str) => parseFloat(str));
+      numbers = expression.split(divideIcon).map((str) => parseFloat(str));
     }
 
     if (numbers.length === 0) return;
@@ -187,6 +203,25 @@ export default function Calculator() {
     return <strong>{formatedItems}</strong>;
   }
 
+  function handleParenthesesClicK() {
+    const openCount = items.filter((item) => item === "(").length;
+    const closeCount = items.filter((item) => item === ")").length;
+
+    if (openCount === closeCount) {
+      setItems((prevState) => [...prevState, "("]);
+    } else if (openCount > closeCount) {
+      if (
+        lastAddedItem !== "(" &&
+        lastAddedItem !== plusIcon &&
+        lastAddedItem !== minusIcon &&
+        lastAddedItem !== productIcon &&
+        lastAddedItem !== divideIcon
+      ) {
+        setItems((prevState) => [...prevState, ")"]);
+      }
+    }
+  }
+
   return (
     <>
       <h1>Calculator</h1>
@@ -197,7 +232,7 @@ export default function Calculator() {
         <div className="btn-wrapper">
           <div className="top">
             <button onClick={handleAllClearClick}>AC</button>
-            <button>()</button>
+            <button onClick={handleParenthesesClicK}>()</button>
             <button>%</button>
             <button onClick={handleDivideClick}>/</button>
           </div>
