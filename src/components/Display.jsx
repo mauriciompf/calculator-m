92361cae3ce631a5bf67345633661bf36d72ebf6
mpfarias <fariasmauricio11@outlook.com>
2024-05-01 18:32:42 -0300

    Update Display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,19 +1,39 @@
 import getExpression from "../utils/getExpression";
 
 import display from "../styles/Display.module.css";
+import { useEffect, useState } from "react";
 
 export default function Display({ expression, errorMessage }) {
+  const [fontSize, setFontSize] = useState("2.25em");
+
   const formattedExpression = getExpression(expression).replace(
     /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
     ","
   );
 
+  useEffect(() => {
+    const expressionWithoutSpace = formattedExpression.replace(/\s/g, "");
+
+    if (expressionWithoutSpace.replace(/\s/g, "").length > 9) {
+      setFontSize("1.25em");
+    } else {
+      setFontSize("2.25em");
+    }
+  }, [formattedExpression]);
+
+  if (formattedExpression.replace(/\s/g, "").length === 34) {
+  }
+
   return (
     <div className={display.wrapper}>
       {errorMessage ? (
-        <strong className={display.error}>{errorMessage}</strong>
+        <strong className={display.error} style={{ fontSize: fontSize }}>
+          {errorMessage}
+        </strong>
       ) : (
-        <strong className={display.expression}>{formattedExpression}</strong>
+        <strong className={display.expression} style={{ fontSize: fontSize }}>
+          {formattedExpression}
+        </strong>
       )}
     </div>
   );
